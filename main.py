5450407b8eaf5b1edfee50f4e3a92dea793d44f7
JoaoFula <hsifula@gmail.com>
2020-09-25 15:19:20 +0100

    Added sound effects and music to the game

--- main.py
+++ main.py
@@ -118,10 +118,10 @@ class snake(object):
                     self.turns.pop(p)
             else:
                 # Check hitboxes with the screen edges
-                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
-                elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0, c.pos[1])
-                elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
-                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0], c.rows-1)
+                if c.dirnx == -1 and c.pos[0] <= 1: c.pos = (c.rows-1, c.pos[1])
+                elif c.dirnx == 1 and c.pos[0] >= c.rows-2: c.pos = (0, c.pos[1])
+                elif c.dirny == 1 and c.pos[1] >= c.rows-2: c.pos = (c.pos[0], 0)
+                elif c.dirny == -1 and c.pos[1] <= 1: c.pos = (c.pos[0], c.rows-1)
                 else: c.move(c.dirnx, c.dirny)
 
     def reset(self, pos):
